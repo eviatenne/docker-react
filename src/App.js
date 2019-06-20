50e3743b71cb41a484d4f8c5168ec716d0f11fc3
Eviatar Tenne <eviatenne@gmail.com>
2019-06-20 15:04:49 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          HI THERE
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React NBA CHAMP Lakers
         </a>
       </header>
     </div>
